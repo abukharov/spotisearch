4d84b06d4026cfd4e17b85d0f408fcbc24eabed8
Alex Bukharov <alex.bukharov@latitudefinancial.com>
2020-04-20 11:51:40 +1000

    refactor: configure linters

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: 'module',
+    },
+    env: {
+        es6: true,
+        browser: true,
+    },
+    plugins: ['prettier', 'svelte3'],
+    overrides: [
+        {
+            files: ['**/*.svelte'],
+            processor: 'svelte3/svelte3',
+        },
+    ],
+    extends: ['prettier', 'google'],
+    rules: {
+        'comma-dangle': [2, 'never'],
+        'no-invalid-this': 1,
+        semi: [0, 'never'],
+        indent: ['error', 4],
+        'object-curly-spacing': [2, 'always'],
+        'prettier/prettier': 1,
+    },
+};
--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
-  export let name
+    export let name;
 </script>
 
-<h1>Hello {name}!</h1>
+<style src="./style.scss" lang="scss">
 
-<style src="./style.scss" lang="scss"></style>
+</style>
+
+<h1>Hello {name}!</h1>

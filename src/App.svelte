2fa9cdbfebb43eecae827008b360618642783641
Alex Bukharov <alex.bukharov@latitudefinancial.com>
2020-04-20 11:26:43 +1000

    Boilerplate app

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let name
+</script>
+
+<h1>Hello {name}!</h1>
+
+<style src="./style.scss" lang="scss"></style>
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+    entry: {
+        bundle: './src/main.js',
+        global: './src/scss/main.scss',
+    },
+    resolve: {
+        alias: {
+            svelte: path.resolve('node_modules', 'svelte'),
+        },
+        extensions: ['.mjs', '.js', '.svelte'],
+        mainFields: ['svelte', 'browser', 'module', 'main'],
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js',
+        chunkFilename: '[name].[id].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                  loader: 'svelte-loader',
+                  options: {
+                    preprocess: require('svelte-preprocess')({
+                      scss: true,
+                      postcss: {
+                        plugins: [require('autoprefixer')]
+                      }
+                    }),
+                    emitCss: true,
+                    hotReload: true
+                  }
+                }
+              },
+            {
+                test: /\.css$/,
+                use: [
+                    /**
+                     * MiniCssExtractPlugin doesn't support HMR.
+                     * For developing, use 'style-loader' instead.
+                     * */
+                    prod ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    mode,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new FixStyleOnlyEntriesPlugin(),
+    ],
+    devtool: prod ? false : 'source-map',
+};
